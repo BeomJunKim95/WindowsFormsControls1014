955153744f4a2f350095195fd58474a20be77160
GDC6 <GDC6@DESKTOP-66QNA8Q>
2020-10-14 18:15:09 +0900

    초기 커밋.

---
+++ WindowsFormsControls1014/frmMember.Designer.cs
@@ -0,0 +1,487 @@
+﻿namespace WindowsFormsControls1014
+{
+	partial class frmMember
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("노드2");
+			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("노드3");
+			System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("노드4");
+			System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("노드5");
+			System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("노드0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+			System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("노드6");
+			System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("노드7");
+			System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("노드8");
+			System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("노드9");
+			System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("노드1", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9});
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.treeView1);
+			this.splitContainer1.Panel2.Controls.Add(this.listView1);
+			this.splitContainer1.Panel2.Controls.Add(this.listBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.comboBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.checkBox3);
+			this.splitContainer1.Panel2.Controls.Add(this.checkBox2);
+			this.splitContainer1.Panel2.Controls.Add(this.checkBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.panel2);
+			this.splitContainer1.Panel2.Controls.Add(this.panel1);
+			this.splitContainer1.Panel2.Controls.Add(this.button4);
+			this.splitContainer1.Panel2.Controls.Add(this.button3);
+			this.splitContainer1.Panel2.Controls.Add(this.button2);
+			this.splitContainer1.Panel2.Controls.Add(this.button1);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox5);
+			this.splitContainer1.Panel2.Controls.Add(this.label5);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox4);
+			this.splitContainer1.Panel2.Controls.Add(this.label4);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox3);
+			this.splitContainer1.Panel2.Controls.Add(this.label3);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox2);
+			this.splitContainer1.Panel2.Controls.Add(this.label2);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.label1);
+			this.splitContainer1.Size = new System.Drawing.Size(1213, 595);
+			this.splitContainer1.SplitterDistance = 191;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Lime;
+			this.button4.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button4.ForeColor = System.Drawing.Color.Red;
+			this.button4.Location = new System.Drawing.Point(654, 460);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(96, 36);
+			this.button4.TabIndex = 29;
+			this.button4.Text = "삭제";
+			this.button4.UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button3.Location = new System.Drawing.Point(526, 460);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(96, 36);
+			this.button3.TabIndex = 28;
+			this.button3.Text = "수정";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button2.Location = new System.Drawing.Point(404, 460);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(96, 36);
+			this.button2.TabIndex = 27;
+			this.button2.Text = "신규";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.button1.Location = new System.Drawing.Point(281, 461);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(96, 36);
+			this.button1.TabIndex = 26;
+			this.button1.Text = "조회";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(342, 354);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(158, 21);
+			this.textBox5.TabIndex = 25;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(280, 357);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(38, 12);
+			this.label5.TabIndex = 24;
+			this.label5.Text = "label5";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(342, 305);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(221, 21);
+			this.textBox4.TabIndex = 23;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(280, 308);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(38, 12);
+			this.label4.TabIndex = 22;
+			this.label4.Text = "label4";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(342, 160);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(349, 121);
+			this.textBox3.TabIndex = 21;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(280, 163);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(38, 12);
+			this.label3.TabIndex = 20;
+			this.label3.Text = "label3";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(342, 111);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(221, 21);
+			this.textBox2.TabIndex = 19;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(280, 114);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(38, 12);
+			this.label2.TabIndex = 18;
+			this.label2.Text = "label2";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(342, 62);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(158, 21);
+			this.textBox1.TabIndex = 17;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(280, 65);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 12);
+			this.label1.TabIndex = 16;
+			this.label1.Text = "label1";
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowTemplate.Height = 23;
+			this.dataGridView1.Size = new System.Drawing.Size(191, 595);
+			this.dataGridView1.TabIndex = 16;
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Location = new System.Drawing.Point(11, 3);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(47, 16);
+			this.radioButton1.TabIndex = 30;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "남자";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(74, 3);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(47, 16);
+			this.radioButton2.TabIndex = 31;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "여자";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(3, 3);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(47, 16);
+			this.radioButton3.TabIndex = 32;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "학생";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.AutoSize = true;
+			this.radioButton4.Location = new System.Drawing.Point(56, 3);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(59, 16);
+			this.radioButton4.TabIndex = 33;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "직장인";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.AutoSize = true;
+			this.radioButton5.Location = new System.Drawing.Point(121, 3);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(47, 16);
+			this.radioButton5.TabIndex = 34;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "백수";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.radioButton2);
+			this.panel1.Controls.Add(this.radioButton1);
+			this.panel1.Location = new System.Drawing.Point(522, 62);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(131, 21);
+			this.panel1.TabIndex = 37;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.radioButton4);
+			this.panel2.Controls.Add(this.radioButton3);
+			this.panel2.Controls.Add(this.radioButton5);
+			this.panel2.Location = new System.Drawing.Point(582, 111);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(177, 21);
+			this.panel2.TabIndex = 38;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(286, 24);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(32, 16);
+			this.checkBox1.TabIndex = 39;
+			this.checkBox1.Text = "S";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.AutoSize = true;
+			this.checkBox2.Location = new System.Drawing.Point(342, 24);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(35, 16);
+			this.checkBox2.TabIndex = 40;
+			this.checkBox2.Text = "M";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.AutoSize = true;
+			this.checkBox3.Location = new System.Drawing.Point(404, 24);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(31, 16);
+			this.checkBox3.TabIndex = 41;
+			this.checkBox3.Text = "L";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "고등학교 졸",
+            "대학교 졸",
+            "대학원 졸",
+            "박사"});
+			this.comboBox1.Location = new System.Drawing.Point(732, 160);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 20);
+			this.comboBox1.TabIndex = 42;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Items.AddRange(new object[] {
+            "가",
+            "나",
+            "다",
+            "라"});
+			this.listBox1.Location = new System.Drawing.Point(733, 193);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.ScrollAlwaysVisible = true;
+			this.listBox1.Size = new System.Drawing.Size(120, 88);
+			this.listBox1.TabIndex = 44;
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(732, 305);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(235, 137);
+			this.listView1.TabIndex = 45;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// treeView1
+			// 
+			this.treeView1.Location = new System.Drawing.Point(32, 24);
+			this.treeView1.Name = "treeView1";
+			treeNode1.Name = "노드2";
+			treeNode1.Text = "노드2";
+			treeNode2.Name = "노드3";
+			treeNode2.Text = "노드3";
+			treeNode3.Name = "노드4";
+			treeNode3.Text = "노드4";
+			treeNode4.Name = "노드5";
+			treeNode4.Text = "노드5";
+			treeNode5.Name = "노드0";
+			treeNode5.Text = "노드0";
+			treeNode6.Name = "노드6";
+			treeNode6.Text = "노드6";
+			treeNode7.Name = "노드7";
+			treeNode7.Text = "노드7";
+			treeNode8.Name = "노드8";
+			treeNode8.Text = "노드8";
+			treeNode9.Name = "노드9";
+			treeNode9.Text = "노드9";
+			treeNode10.Name = "노드1";
+			treeNode10.Text = "노드1";
+			this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode10});
+			this.treeView1.Size = new System.Drawing.Size(172, 548);
+			this.treeView1.TabIndex = 46;
+			// 
+			// frmMember
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1213, 595);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "frmMember";
+			this.Text = "frmMember";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
---
+++ WindowsFormsControls1014/frmProduct.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsControls1014
+{
+	partial class frmProduct
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmProduct));
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker2);
+			this.splitContainer1.Panel1.Controls.Add(this.dateTimePicker1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.monthCalendar1);
+			this.splitContainer1.Panel2.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.numericUpDown1);
+			this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+			this.splitContainer1.SplitterDistance = 71;
+			this.splitContainer1.SplitterWidth = 10;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePicker1.Location = new System.Drawing.Point(110, 33);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(105, 21);
+			this.dateTimePicker1.TabIndex = 0;
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+			this.dateTimePicker2.Location = new System.Drawing.Point(251, 33);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(111, 21);
+			this.dateTimePicker2.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(478, 34);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(506, 64);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(66, 21);
+			this.numericUpDown1.TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(95, 64);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 21);
+			this.textBox1.TabIndex = 1;
+			// 
+			// errorProvider1
+			// 
+			this.errorProvider1.ContainerControl = this;
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(70, 130);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(439, 130);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(182, 159);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// frmProduct
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.splitContainer1);
+			this.Name = "frmProduct";
+			this.Text = "frmProduct";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.Panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.ErrorProvider errorProvider1;
+	}
+}
